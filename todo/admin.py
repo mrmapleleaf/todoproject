5a96714f012a56a33be308ba08f56a1c25399437
mrmapleleaf <kaple.de.96@gmail.com>
2025-11-06 22:26:47 +0900

    admin機能の追加

--- todo/admin.py
+++ todo/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
+from .models import ToDoItem
 
-# from .models import TodoItem
 
-
-# # Register your models here.
-# @admin.register(TodoItem)
-# class TodoItemAdmin(admin.ModelAdmin):
-#     list_display = ("id", "title", "is_completed", "created_at", "updated_at")
-#     list_filter = "is_completed"
-#     search_fields = ("title",)
+# Register your models here.
+@admin.register(ToDoItem)
+class ToDoItemAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "is_completed", "created_at", "updated_at")
+    list_filter = [
+        "is_completed",
+    ]
+    search_fields = [
+        "title",
+    ]
