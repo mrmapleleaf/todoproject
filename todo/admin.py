822e63fe4351fe91551bf4d08f19d489a7d79cc2
mrmapleleaf <kaple.de.96@gmail.com>
2025-11-06 22:15:41 +0900

    モデルを追加

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+# from .models import TodoItem
+
+
+# # Register your models here.
+# @admin.register(TodoItem)
+# class TodoItemAdmin(admin.ModelAdmin):
+#     list_display = ("id", "title", "is_completed", "created_at", "updated_at")
+#     list_filter = "is_completed"
+#     search_fields = ("title",)
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class ToDoItem(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.id} ： {self.title}"
