5aab956b2e9a70664be9a119aaf33e6d46d528cf
mrmapleleaf <kaple.de.96@gmail.com>
2025-11-08 00:44:37 +0900

    todoリストの一覧を表示する

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import ToDoItem
+
 
 # Create your views here.
+def todo_list(request):
+    todos = ToDoItem.objects.all().order_by("-created_at")
+    return render(request, "todo/todo_list.html", {"todos": todos})
