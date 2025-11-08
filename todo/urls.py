fecbe3bbb4fe399098dd780d9e492408198ac59a
mrmapleleaf <kaple.de.96@gmail.com>
2025-11-08 15:48:09 +0900

    タスクの削除、ステータスの変更機能を追加

--- todo/urls.py
+++ todo/urls.py
@@ -4,4 +4,10 @@ from . import views
 urlpatterns = [
     path("", views.todo_list, name="todo_list"),
     path("add/", views.todo_create, name="todo_create"),
+    path("delete/<int:todo_id>/", views.todo_delete, name="todo_delete"),
+    path(
+        "update_status/<int:todo_id>/",
+        views.todo_update_status,
+        name="todo_update_status",
+    ),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import ToDoItem
+from django.views.decorators.http import require_POST
 
 
 # Create your views here.
@@ -15,3 +16,18 @@ def todo_create(request):
             ToDoItem.objects.create(title=title)
             return redirect("todo_list")
     return render(request, "todo/todo_form.html")
+
+
+def todo_delete(request, todo_id):
+    todo = ToDoItem.objects.get(id=todo_id)
+    todo.delete()
+    return redirect("todo_list")
+
+
+@require_POST
+def todo_update_status(request, todo_id):
+    # todo = ToDoItem.objects.get(id=todo_id)
+    todo = get_object_or_404(ToDoItem, id=todo_id)
+    todo.is_completed = not todo.is_completed
+    todo.save()
+    return redirect("todo_list")
